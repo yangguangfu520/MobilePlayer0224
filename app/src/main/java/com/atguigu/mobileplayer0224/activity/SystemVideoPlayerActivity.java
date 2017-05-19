88bbc204555aa83ecbd26cbe8deffe19eb0bc4aa
yangguangfu520 <ygf@atguigu.com>
2017-05-19 17:26:48 +0800

    快速实现简单播放器

---
+++ app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
@@ -0,0 +1,64 @@
+package com.atguigu.mobileplayer0224.activity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.atguigu.mobileplayer0224.R;
+
+public class SystemVideoPlayerActivity extends AppCompatActivity {
+    private VideoView vv;
+    private Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_video_player);
+        vv = (VideoView)findViewById(R.id.vv);
+
+        //得到播放地址
+        uri = getIntent().getData();
+
+
+
+        //设置播放器三个监听：播放准备好的监听，播放完成的监听，播放出错的监听
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            //底层准备播放完成的时候回调
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                vv.start();//开始播放
+            }
+        });
+
+        vv.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(SystemVideoPlayerActivity.this, "播放出错了哦", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        //设置监听播放完成
+        vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(SystemVideoPlayerActivity.this, "视频播放完成", Toast.LENGTH_SHORT).show();
+                finish();//退出当前页面
+            }
+        });
+
+        //设置播放地址
+        vv.setVideoURI(uri);
+
+        //设置控制面板
+        vv.setMediaController(new MediaController(this));
+
+
+
+
+    }
+}
--- app/src/main/java/com/atguigu/mobileplayer0224/pager/LocalVideoPager.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/pager/LocalVideoPager.java
@@ -1,6 +1,7 @@
 package com.atguigu.mobileplayer0224.pager;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.atguigu.mobileplayer0224.R;
+import com.atguigu.mobileplayer0224.activity.SystemVideoPlayerActivity;
 import com.atguigu.mobileplayer0224.adapter.LocalVideoAdapter;
 import com.atguigu.mobileplayer0224.domain.MediaItem;
 import com.atguigu.mobileplayer0224.fragment.BaseFragment;
@@ -50,6 +52,10 @@ public class LocalVideoPager extends BaseFragment {
 
                 MediaItem item = adapter.getItem(position);
                 Toast.makeText(context, ""+item.toString(), Toast.LENGTH_SHORT).show();
+                //把系统的播放器调起来
+                Intent intent = new Intent(context,SystemVideoPlayerActivity.class);
+                intent.setDataAndType(Uri.parse(item.getData()),"video/*");
+                startActivity(intent);
 
 
             }
