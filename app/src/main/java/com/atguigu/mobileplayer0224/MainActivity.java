f09be721c6a2235011e1181b07a88427dfb3b08c
yangguangfu520 <ygf@atguigu.com>
2017-05-19 11:39:13 +0800

    底部RadioGroup

--- app/src/main/java/com/atguigu/mobileplayer0224/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/MainActivity.java
@@ -2,12 +2,17 @@ package com.atguigu.mobileplayer0224;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rg_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化控件
+        rg_main = (RadioGroup)findViewById(R.id.rg_main);
     }
 }
