fa4534a6a45698c107288790b695405af3896d3e
yangguangfu520 <ygf@atguigu.com>
2017-05-20 11:03:28 +0800

    播放器控制栏底部的完成

--- app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
@@ -4,21 +4,105 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.atguigu.mobileplayer0224.R;
 
-public class SystemVideoPlayerActivity extends AppCompatActivity {
+public class SystemVideoPlayerActivity extends AppCompatActivity implements View.OnClickListener {
     private VideoView vv;
     private Uri uri;
+
+    private LinearLayout llTop;
+    private TextView tvName;
+    private ImageView ivBattery;
+    private TextView tvSystemTime;
+    private Button btnVoice;
+    private SeekBar seekbarVoice;
+    private Button btnSwitchPlayer;
+    private LinearLayout llBottom;
+    private TextView tvCurrentTime;
+    private SeekBar seekbarVideo;
+    private TextView tvDuration;
+    private Button btnExit;
+    private Button btnPre;
+    private Button btnStartPause;
+    private Button btnNext;
+    private Button btnSwitchScreen;
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2017-05-20 11:01:51 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        setContentView(R.layout.activity_system_video_player);
+        llTop = (LinearLayout)findViewById( R.id.ll_top );
+        tvName = (TextView)findViewById( R.id.tv_name );
+        ivBattery = (ImageView)findViewById( R.id.iv_battery );
+        tvSystemTime = (TextView)findViewById( R.id.tv_system_time );
+        btnVoice = (Button)findViewById( R.id.btn_voice );
+        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
+        btnSwitchPlayer = (Button)findViewById( R.id.btn_switch_player );
+        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
+        tvCurrentTime = (TextView)findViewById( R.id.tv_current_time );
+        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
+        tvDuration = (TextView)findViewById( R.id.tv_duration );
+        btnExit = (Button)findViewById( R.id.btn_exit );
+        btnPre = (Button)findViewById( R.id.btn_pre );
+        btnStartPause = (Button)findViewById( R.id.btn_start_pause );
+        btnNext = (Button)findViewById( R.id.btn_next );
+        btnSwitchScreen = (Button)findViewById( R.id.btn_switch_screen );
+        vv = (VideoView)findViewById(R.id.vv);
+
+        btnVoice.setOnClickListener( this );
+        btnSwitchPlayer.setOnClickListener( this );
+        btnExit.setOnClickListener( this );
+        btnPre.setOnClickListener( this );
+        btnStartPause.setOnClickListener( this );
+        btnNext.setOnClickListener( this );
+        btnSwitchScreen.setOnClickListener( this );
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2017-05-20 11:01:51 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == btnVoice ) {
+            // Handle clicks for btnVoice
+        } else if ( v == btnSwitchPlayer ) {
+            // Handle clicks for btnSwitchPlayer
+        } else if ( v == btnExit ) {
+            // Handle clicks for btnExit
+        } else if ( v == btnPre ) {
+            // Handle clicks for btnPre
+        } else if ( v == btnStartPause ) {
+            // Handle clicks for btnStartPause
+        } else if ( v == btnNext ) {
+            // Handle clicks for btnNext
+        } else if ( v == btnSwitchScreen ) {
+            // Handle clicks for btnSwitchScreen
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_system_video_player);
-        vv = (VideoView)findViewById(R.id.vv);
+
+        findViews();
+
 
         //得到播放地址
         uri = getIntent().getData();
@@ -56,7 +140,7 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
         vv.setVideoURI(uri);
 
         //设置控制面板
-        vv.setMediaController(new MediaController(this));
+//        vv.setMediaController(new MediaController(this));
 
 
 
