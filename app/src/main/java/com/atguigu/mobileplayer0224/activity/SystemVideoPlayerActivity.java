eee8ebd8788417da6a4a88419b5faa15646bd093
yangguangfu520 <ygf@atguigu.com>
2017-05-22 11:01:10 +0800

    判断uri是否网络资源&支持显示缓冲进度&手机播放电脑tomcat资源-

--- app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/activity/SystemVideoPlayerActivity.java
@@ -92,6 +92,11 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
     private int maxVoice;
     //是否静音
     private boolean isMute = false;
+    /**
+     * 是否是网络资源
+     *
+     */
+    private boolean isNetUri;
 
     /**
      * Find the Views in the layout<br />
@@ -262,6 +267,16 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
                     //得到系统时间
                     tvSystemTime.setText(getSystemTime());
 
+                    //设置视频缓存效果
+                    if(isNetUri){
+                        int bufferPercentage = vv.getBufferPercentage();//0~100;
+                        int totalBuffer = bufferPercentage*seekbarVideo.getMax();
+                        int secondaryProgress =totalBuffer/100;
+                        seekbarVideo.setSecondaryProgress(secondaryProgress);
+                    }else{
+                        seekbarVideo.setSecondaryProgress(0);
+                    }
+
                     //循环发消息
                     sendEmptyMessageDelayed(PROGRESS, 1000);
 
@@ -309,11 +324,13 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
             MediaItem mediaItem = mediaItems.get(position);
             tvName.setText(mediaItem.getName());
             vv.setVideoPath(mediaItem.getData());
+            isNetUri =  utils.isNetUri(mediaItem.getData());
 
         } else if (uri != null) {
             //设置播放地址
             vv.setVideoURI(uri);
             tvName.setText(uri.toString());
+            isNetUri =  utils.isNetUri(uri.toString());
         }
 
         setButtonStatus();
@@ -610,8 +627,10 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
         if (position > 0) {
             //还是在列表范围内容
             MediaItem mediaItem = mediaItems.get(position);
+            isNetUri =  utils.isNetUri(mediaItem.getData());
             vv.setVideoPath(mediaItem.getData());
             tvName.setText(mediaItem.getName());
+
 
             //设置按钮状态
             setButtonStatus();
@@ -626,6 +645,7 @@ public class SystemVideoPlayerActivity extends AppCompatActivity implements View
         if (position < mediaItems.size()) {
             //还是在列表范围内容
             MediaItem mediaItem = mediaItems.get(position);
+            isNetUri =  utils.isNetUri(mediaItem.getData());
             vv.setVideoPath(mediaItem.getData());
             tvName.setText(mediaItem.getName());
 
--- app/src/main/java/com/atguigu/mobileplayer0224/utils/Utils.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/utils/Utils.java
@@ -5,36 +5,52 @@ import java.util.Locale;
 
 public class Utils {
 
-	private StringBuilder mFormatBuilder;
-	private Formatter mFormatter;
+    private StringBuilder mFormatBuilder;
+    private Formatter mFormatter;
 
-	public Utils() {
-		// 转换成字符串的时间
-		mFormatBuilder = new StringBuilder();
-		mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
+    public Utils() {
+        // 转换成字符串的时间
+        mFormatBuilder = new StringBuilder();
+        mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
 
-	}
+    }
 
-	/**
-	 * 把毫秒转换成：1:20:30这里形式
-	 * @param timeMs
-	 * @return
-	 */
-	public String stringForTime(int timeMs) {
-		int totalSeconds = timeMs / 1000;
-		int seconds = totalSeconds % 60;
+    /**
+     * 把毫秒转换成：1:20:30这里形式
+     *
+     * @param timeMs
+     * @return
+     */
+    public String stringForTime(int timeMs) {
+        int totalSeconds = timeMs / 1000;
+        int seconds = totalSeconds % 60;
 
-		int minutes = (totalSeconds / 60) % 60;
+        int minutes = (totalSeconds / 60) % 60;
 
-		int hours = totalSeconds / 3600;
+        int hours = totalSeconds / 3600;
 
-		mFormatBuilder.setLength(0);
-		if (hours > 0) {
-			return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds)
-					.toString();
-		} else {
-			return mFormatter.format("%02d:%02d", minutes, seconds).toString();
-		}
-	}
+        mFormatBuilder.setLength(0);
+        if (hours > 0) {
+            return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds)
+                    .toString();
+        } else {
+            return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+        }
+    }
 
+    /**
+     * 是否是网络资源
+     * @param data
+     * @return
+     */
+    public boolean isNetUri(String data) {
+        boolean isNetUri = false;
+        if (data != null) {
+            if (data.toLowerCase().startsWith("http") || data.toLowerCase().startsWith("mms") || data.toLowerCase().startsWith("rtsp")) {
+                //网络资源
+                isNetUri = true;
+            }
+        }
+        return isNetUri;
+    }
 }
