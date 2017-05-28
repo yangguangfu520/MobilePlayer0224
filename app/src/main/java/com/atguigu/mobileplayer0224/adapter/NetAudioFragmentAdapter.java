8178ad7aac3c1c9aef150e23017b5dd3449dda2c
yangguangfu520 <ygf@atguigu.com>
2017-05-28 15:49:59 +0800

    gif的ViewHolder

--- app/src/main/java/com/atguigu/mobileplayer0224/adapter/NetAudioFragmentAdapter.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/adapter/NetAudioFragmentAdapter.java
@@ -14,6 +14,8 @@ import com.atguigu.mobileplayer0224.utils.Utils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
+import org.xutils.common.util.DensityUtil;
+import org.xutils.image.ImageOptions;
 import org.xutils.x;
 
 import java.util.List;
@@ -184,7 +186,7 @@ public class NetAudioFragmentAdapter extends BaseAdapter {
                     gifHolder = (GifHolder) convertView.getTag();
                 }
 
-//                gifHolder.setData(mediaItem);
+                gifHolder.setData(mediaItem);
 
                 break;
             case TYPE_AD://软件广告
@@ -357,9 +359,40 @@ public class NetAudioFragmentAdapter extends BaseAdapter {
         }
     }
 
-    class GifHolder {
+    class GifHolder extends BaseViewHolder{
+        TextView tvContext;
+        ImageView ivImageGif;
+        private ImageOptions imageOptions;
 
-        public GifHolder(View convertView) {
+        GifHolder(View convertView) {
+            super(convertView);
+            //中间公共部分 -所有的都有
+            tvContext = (TextView) convertView.findViewById(R.id.tv_context);
+            ivImageGif = (ImageView) convertView.findViewById(R.id.iv_image_gif);
+
+            imageOptions = new ImageOptions.Builder()
+                    //包裹类型
+                    .setSize(ViewGroup.LayoutParams.WRAP_CONTENT, -2)
+                    //设置圆角
+                    .setRadius(DensityUtil.dip2px(5))
+                    .setIgnoreGif(false)//是否忽略gif图。false表示不忽略。不写这句，默认是true
+                    .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                    .setLoadingDrawableId(R.drawable.video_default)
+                    .setFailureDrawableId(R.drawable.video_default)
+                    .build();
+
+        }
+
+        public void setData(NetAudioBean.ListBean mediaItem) {
+            super.setData(mediaItem);
+            //设置文本-所有的都有
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+
+            //下面是gif
+            if (mediaItem.getGif() != null && mediaItem.getGif() != null && mediaItem.getGif().getImages() != null) {
+//                Glide.with(context).load(mediaItem.getGif().getImages().get(0)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(ivImageGif);
+                x.image().bind(ivImageGif, mediaItem.getGif().getImages().get(0), imageOptions);
+            }
 
         }
     }
